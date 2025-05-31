96a80a5bd0abbb1002ba33cdb54257ff49eabe1b
Alexander Bondarenco <sasha.199568@gmail.com>
2025-05-31 21:15:48 +0300

    Paths

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,14 @@
+package com.app
+
+import com.utils.Printer
+
+fun main() {
+    val name = "Kotlin"
+    val message = "Hello, " + name + "!"
+    val printer = Printer(message)
+    printer.printMessage()
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
